169ed016d9fbbef89b8724287efe85735b677b5d
[YokYukYik] <clubyoyo12@gmail.com>
2021-05-22 12:37:38 +0700

    update

---
+++ fonts/THSarabunNew.js
@@ -0,0 +1,8 @@
+module.exports = {
+	THSarabunNew: {
+		normal: __dirname + '/THSarabunNew/THSarabunNew.ttf',
+		bold: __dirname + '/THSarabunNew/THSarabunNew-Bold.ttf',
+		italics: __dirname + '/THSarabunNew/THSarabunNew-Italic.ttf',
+		bolditalics: __dirname + '/THSarabunNew/THSarabunNew-BoldItalic.ttf'
+	}
+};
